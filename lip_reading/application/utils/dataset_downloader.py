47ed8ba93f1e2326de97dc4986acbb67b38f2d5a
uiyun-kim <66242193+uiyun-kim@users.noreply.github.com>
2020-06-26 17:53:00 +0900

    nothing

--- lip_reading/application/utils/dataset_downloader.py
+++ lip_reading/application/utils/dataset_downloader.py
@@ -5,7 +5,7 @@ import os
 def Dataset_downloader(name='gen1'):
     datasets = {'gen1':'1qeYPML6472pszI1auEOyboaXS5Rrlora'}
 
-    gdd.download_file_from_google_drive(file_id=datasets['gen1'],
+    gdd.download_file_from_google_drive(file_id=datasets[name],
                                         dest_path='./data/dataset.zip',
                                         unzip=True)
     os.remove('./data/dataset.zip')
